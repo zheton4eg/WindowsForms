93e271f07e11fd9715d6ded5656aaab86c1f3ecd
zheka <zheka@HOME-PC>
2024-12-14 18:41:04 +0300

    Add project files.

---
+++ Clock/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Clock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.CBShowDate = new System.Windows.Forms.CheckBox();
+            this.btnHideControls = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(1, 4);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(231, 55);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "labelTime";
+            this.labelTime.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.labelTime_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CBShowDate
+            // 
+            this.CBShowDate.AutoSize = true;
+            this.CBShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CBShowDate.Location = new System.Drawing.Point(44, 183);
+            this.CBShowDate.Name = "CBShowDate";
+            this.CBShowDate.Size = new System.Drawing.Size(176, 29);
+            this.CBShowDate.TabIndex = 1;
+            this.CBShowDate.Text = "Показать дату";
+            this.CBShowDate.UseVisualStyleBackColor = true;
+            this.CBShowDate.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnHideControls
+            // 
+            this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnHideControls.Location = new System.Drawing.Point(86, 240);
+            this.btnHideControls.Name = "btnHideControls";
+            this.btnHideControls.Size = new System.Drawing.Size(80, 30);
+            this.btnHideControls.TabIndex = 2;
+            this.btnHideControls.Text = "Hide controls";
+            this.btnHideControls.UseVisualStyleBackColor = true;
+            this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 406);
+            this.Controls.Add(this.btnHideControls);
+            this.Controls.Add(this.CBShowDate);
+            this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Clock PV_319";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox CBShowDate;
+        private System.Windows.Forms.Button btnHideControls;
+    }
+}
+
---
+++ Clock/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt",System.Globalization.CultureInfo.InvariantCulture);
+            if(CBShowDate.Checked)
+            {
+                labelTime.Text += "\n"; 
+                labelTime.Text += DateTime.Now.ToString("yyyy.MM.dd"); 
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHideControls_Click(object sender, EventArgs e)
+        {
+            CBShowDate.Visible = false;
+            btnHideControls.Visible = false;
+            this.TransparencyKey = this.BackColor;
+            this.FormBorderStyle=FormBorderStyle.None;
+            labelTime.BackColor = Color.AliceBlue;
+            this.ShowInTaskbar = false;
+        }
+
+        private void labelTime_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show
+                            (
+                this,
+                "Вы два раза целкнули мышью по времени, и теперь вы управляете временем"
+                , "Info",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+        }
+    }
+}
