c853d07dd6ee6efc432dfc4ec1e29db9f91bb9b5
zheka <zheka@HOME-PC>
2024-12-19 17:26:52 +0300

    classwork 17.12.24 DONE

---
+++ Clock/ChooseFont.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Clock
+{
+    partial class nudFontSize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(nudFontSize));
+            this.cbFonts = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelChooseFont = new System.Windows.Forms.Label();
+            this.labelFontSize = new System.Windows.Forms.Label();
+            this.labelExample = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCANCEL = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbFonts
+            // 
+            this.cbFonts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFonts.FormattingEnabled = true;
+            this.cbFonts.Location = new System.Drawing.Point(12, 53);
+            this.cbFonts.Name = "cbFonts";
+            this.cbFonts.Size = new System.Drawing.Size(254, 21);
+            this.cbFonts.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(272, 54);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // labelChooseFont
+            // 
+            this.labelChooseFont.AutoSize = true;
+            this.labelChooseFont.Location = new System.Drawing.Point(13, 34);
+            this.labelChooseFont.Name = "labelChooseFont";
+            this.labelChooseFont.Size = new System.Drawing.Size(70, 13);
+            this.labelChooseFont.TabIndex = 2;
+            this.labelChooseFont.Text = "ChooseFont :";
+            // 
+            // labelFontSize
+            // 
+            this.labelFontSize.AutoSize = true;
+            this.labelFontSize.Location = new System.Drawing.Point(272, 34);
+            this.labelFontSize.Name = "labelFontSize";
+            this.labelFontSize.Size = new System.Drawing.Size(55, 13);
+            this.labelFontSize.TabIndex = 3;
+            this.labelFontSize.Text = "Font size :";
+            // 
+            // labelExample
+            // 
+            this.labelExample.AutoSize = true;
+            this.labelExample.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelExample.Location = new System.Drawing.Point(13, 111);
+            this.labelExample.Name = "labelExample";
+            this.labelExample.Size = new System.Drawing.Size(140, 37);
+            this.labelExample.TabIndex = 4;
+            this.labelExample.Text = "Example";
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(191, 189);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCANCEL
+            // 
+            this.btnCANCEL.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCANCEL.Location = new System.Drawing.Point(272, 189);
+            this.btnCANCEL.Name = "btnCANCEL";
+            this.btnCANCEL.Size = new System.Drawing.Size(75, 23);
+            this.btnCANCEL.TabIndex = 6;
+            this.btnCANCEL.Text = "Cancel";
+            this.btnCANCEL.UseVisualStyleBackColor = true;
+            // 
+            // nudFontSize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 224);
+            this.Controls.Add(this.btnCANCEL);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.labelExample);
+            this.Controls.Add(this.labelFontSize);
+            this.Controls.Add(this.labelChooseFont);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.cbFonts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "nudFontSize";
+            this.Text = "ChooseFont";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbFonts;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelChooseFont;
+        private System.Windows.Forms.Label labelFontSize;
+        private System.Windows.Forms.Label labelExample;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCANCEL;
+    }
+}
